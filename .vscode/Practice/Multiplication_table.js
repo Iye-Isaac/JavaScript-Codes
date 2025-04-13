fb966a00c9c197bbf03a79aa1c47fba9dc23428b
Iyeoluwa <iyeoluwa.isaac@gmail.com>
2025-04-13 23:09:40 +0100

    Web Development tutorial

---
+++ .vscode/Practice/Multiplication_table.js
@@ -0,0 +1,25 @@
+//Print Multiplication Table
+// Write a function printTable(n) that prints the multiplication table of any number n up to 12.
+//
+// Use a loop inside the function.
+
+//MY SOLUTION
+
+function Multiplication(n) {
+    const num1 = n * 1;
+    const num2 = n * 2;
+    const num3 = n * 3;
+    const num4 = n * 4;
+    const num5 = n * 5;
+    const num6 = n * 6;
+    const num7 = n * 7;
+    const num8 = n * 8;
+    const num9 = n * 9;
+    const num10 = n * 10;
+    const num11 = n * 11;
+    const num12 = n * 12;
+
+    const result = (num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, num12);
+}
+var output = Multiplication(2);
+console.log("Multiplication is: "+ output);
---
+++ .vscode/Practice/Multiplication_tableedited.js
@@ -0,0 +1,10 @@
+function multiplication(n) {
+    let result = ""; // to hold the full table as a string
+    for (let i = 1; i <= 12; i++) {
+        result+= `${n} x ${i} = ${n * i}\n`;
+    }
+    return result;
+}
+
+const output = multiplication(2);
+console.log("Multiplication Table:\n" + output);
